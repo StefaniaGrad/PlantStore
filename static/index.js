226f937962af0813adbfbf5c4465cffca6df16cc
stefaniagrad <stefaniagrad07@gmail.com>
2021-10-21 14:44:17 +0300

    ddd

--- static/index.js
+++ static/index.js
@@ -1,7 +1,15 @@
 function deleteProduct(productId) {
-  fetch("/delete-product", {
+  fetch("/delete-order", {
     method: "POST",
     body: JSON.stringify({ productId: productId }),
+  }).then((_res) => {
+    window.location.href = "/admin";
+  });
+}
+function updateOrder(orderId) {
+  fetch("/update-order", {
+    method: "POST",
+    body: JSON.stringify({ orderId: orderId }),
   }).then((_res) => {
     window.location.href = "/admin";
   });
